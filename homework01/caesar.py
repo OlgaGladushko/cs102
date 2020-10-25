90450b0e1551be1d30d0f4fce35d769cb118d3b0
OlgaGladushko <Olga.gladushko@gmail.com>
2020-10-25 17:11:07 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,24 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letters = "abcdefghigklmnopqrstuvwxyz"
+    s = list(plaintext)
+    for i in s:
+        if i in letters:
+            n = letters.index(i)
+            x = n + shift
+            if x > len(letters):
+                x = x % 26
+            ciphertext = ciphertext + letters[x]
+        elif i in LETTERS:
+            n = LETTERS.index(i)
+            x = n + shift
+            if x > len(LETTERS):
+                x = x % 26
+            ciphertext = ciphertext + LETTERS[x]
+        else:
+            ciphertext = ciphertext + i 
     return ciphertext
 
 
@@ -33,14 +50,25 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letters = "abcdefghigklmnopqrstuvwxyz"
+    s = list(ciphertext)
+    for i in s:
+        if i in letters:
+            n = letters.index(i)
+            x = n - shift
+            if x < 0:
+                x = x + 26
+            plaintext = plaintext + letters[x]
+        elif i in LETTERS:
+            n = LETTERS.index(i)
+            x = n - shift
+            if x < 0:
+                x = x + 26
+            plaintext = plaintext + LETTERS[x]
+        else:
+            plaintext = plaintext + i 
     return plaintext
 
 
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+
